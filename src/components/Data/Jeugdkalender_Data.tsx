475e53e045e94bd5ea7489bee0aedb100567b4d9
deon-lee-personal <deon.lee@hotmail.com>
2023-08-01 11:55:22 +0200

    Githug-pages added

--- src/components/Data/Jeugdkalender_Data.tsx
+++ src/components/Data/Jeugdkalender_Data.tsx
@@ -1,51 +1,260 @@
 const schaakles = "Schaakles voor alle groepen";
 const interclub = "Geen les vanaf STAP 4 (Interclub)";
 const intern = "Intern toernooi";
+const vakantie = "Geen les wegens vakantie";
+const criterium = "Jeugdschaakcriterium";
+const examens = "Stappenexamens en vrij schaken";
 
 const Jeugdkalender_data = [
   {
-    event: schaakles,
-    date: "30-09-2023",
+    event: "Verwelkoming voor de nieuwe leden. Vrij schaken en stappenexamens",
+    date: "zondag 3 september 2023",
+    folder: "",
+  },
+  {
+    event: "Voorlopige groepsindeling",
+    date: "zondag 10 september 2023",
+    folder: "",
+  },
+  {
+    event: criterium,
+    date: "zaterdag 16 september 2023",
     folder: "",
   },
   {
     event: schaakles,
-    date: "7-10-2023",
-    folder: "",
-  },
-  {
-    event: schaakles,
-    date: "14-10-2023",
-    folder: "",
-  },
-  {
-    event: schaakles,
-    date: "21-10-2023",
+    date: "zondag 17 september 2023",
     folder: "",
   },
   {
     event: interclub,
-    date: "28-10-2023",
+    date: "zondag 24 september 2023",
     folder: "",
   },
   {
     event: schaakles,
-    date: "4-11-2023",
+    date: "zondag 1 oktober 2023",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 8 oktober 2023",
     folder: "",
   },
   {
     event: interclub,
-    date: "11-11-2023",
+    date: "zondag 15 oktober 2023",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 22 oktober 2023",
+    folder: "",
+  },
+  {
+    event: vakantie,
+    date: "zondag 29 oktober 2023",
+    folder: "",
+  },
+  {
+    event: criterium,
+    date: "woensdag 1 november 2023",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 5 november 2023",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 12 november 2023",
+    folder: "",
+  },
+  {
+    event: criterium,
+    date: "zondag 12 november 2023",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 19 november 2023",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 26 november 2023",
+    folder: "",
+  },
+  {
+    event: criterium,
+    date: "zondag 26 november 2023",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 3 december 2023",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 10 december 2023",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 17 december 2023",
+    folder: "",
+  },
+  {
+    event: vakantie,
+    date: "zondag 24 december 2023",
+    folder: "",
+  },
+  {
+    event: criterium,
+    date: "(woensdag 27 - donderdag 28) december 2023",
+    folder: "",
+  },
+  {
+    event: vakantie,
+    date: "zondag 31 december 2023",
     folder: "",
   },
   {
     event: intern,
-    date: "18-11-2023",
+    date: "zondag 7 januari 2024",
+    folder: "",
+  },
+  {
+    event:
+      "Stapjestornooi voor Stap 1 tot en met 3+ Schaakles voor Stap 4 en hoger",
+    date: "zondag 14 januari 2024",
     folder: "",
   },
   {
     event: schaakles,
-    date: "25-11-2023",
+    date: "zondag 21 januari 2024",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 28 januari 2024",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 4 februari 2024",
+    folder: "",
+  },
+  {
+    event: vakantie,
+    date: "zondag 11 februari 2024",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 18 februari 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 25 februari 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 3 maart 2024",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 10 maart 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 17 maart 2024",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 24 maart 2024",
+    folder: "",
+  },
+  {
+    event: vakantie,
+    date: "zondag 31 maart 2024",
+    folder: "",
+  },
+  {
+    event: vakantie,
+    date: "zondag 7 april 2024",
+    folder: "",
+  },
+  {
+    event: intern,
+    date: "zondag 14 april 2024",
+    folder: "",
+  },
+  {
+    event: interclub,
+    date: "zondag 21 april 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 28 april 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 5 mei 2024",
+    folder: "",
+  },
+  {
+    event: "Belgisch Jeugdkampioenschap Turnhout",
+    date: "(donderdag 9 - zondag 12) mei 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 12 mei 2024",
+    folder: "",
+  },
+  {
+    event: vakantie,
+    date: "zondag 19 mei 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 26 mei 2024",
+    folder: "",
+  },
+  {
+    event: schaakles,
+    date: "zondag 2 juni 2024",
+    folder: "",
+  },
+  {
+    event: examens,
+    date: "zondag 9 juni 2024",
+    folder: "",
+  },
+  {
+    event: examens,
+    date: "zondag 16 juni 2024",
+    folder: "",
+  },
+  {
+    event: examens,
+    date: "zondag 23 juni 2024",
+    folder: "",
+  },
+  {
+    event: intern,
+    date: "zondag 30 juni 2024",
     folder: "",
   },
 ];
