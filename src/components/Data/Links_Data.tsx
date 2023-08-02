6b58b3259e2f6a1a10ab79fc6041df91bf59245e
deon-lee-personal <deon.lee@hotmail.com>
2023-08-02 17:06:01 +0200

    Links done

--- src/components/Data/Links_Data.tsx
+++ src/components/Data/Links_Data.tsx
@@ -28,6 +28,113 @@ const Links_data = {
       link: "https://sites.google.com/view/vlaams-jeugdschaakcriterium/homepage",
     },
   ],
+
+  Clubs: [
+    {
+      name: "Koninklijke Hasseltse Schaakclub O'Kelly Hasselt",
+      link: "http://www.schaakclub-okelly.be/",
+    },
+    {
+      name: "Koninklijke Schaakclub Pion Aalst",
+      link: "https://pionaalst.be/",
+    },
+    {
+      name: "Koninklijke Waregemse Schaakvereniging Luctor et Emergo",
+      link: "https://www.kwsle.be/",
+    },
+    {
+      name: "Schaakkring het Witte Paard",
+      link: "http://www.skhetwitpaard.be/",
+    },
+    {
+      name: "Schaakclub Caballos Zottegem",
+      link: "http://www.caballos.be/",
+    },
+    {
+      name: "Schaakvereniging Sliedrecht",
+      link: "http://www.schakendsliedrecht.nl/",
+    },
+    {
+      name: "Schaakvereniging Promotie Zoetermeer",
+      link: "http://www.svpromotie.nl/",
+    },
+  ],
+
+  Schaakwinkels: [
+    {
+      name: "Chessconsult",
+      link: "https://chessconsult.be/",
+    },
+    {
+      name: "De Denksportkampioen",
+      link: "https://www.denksportkampioen.be/",
+    },
+    {
+      name: "Maison Des Échechs",
+      link: "https://maisondesechecs.be/nl/",
+    },
+    {
+      name: "Schaakboeken Verzendhuis De Beste Zet",
+      link: "https://www.debestezet.nl/catalog/index.php",
+    },
+  ],
+
+  Leren: [
+    {
+      name: "ChessKid",
+      link: "https://www.chesskid.com/nl/login",
+    },
+    {
+      name: "Chessity",
+      link: "https://www.chessity.com/nl",
+    },
+    {
+      name: "Schaakzone",
+      link: "https://www.schaakzone.nl/schaakopeningen.php",
+    },
+  ],
+
+  Internationaal: [
+    {
+      name: "FIDE",
+      link: "https://www.fide.com/",
+    },
+    {
+      name: "New In Chess",
+      link: "https://www.newinchess.com/",
+    },
+    {
+      name: "ChessBase",
+      link: "https://en.chessbase.com/",
+    },
+    {
+      name: "ChessCafe",
+      link: "https://chesscafe.com/",
+    },
+    {
+      name: "Europe Echecs",
+      link: "https://www.europe-echecs.com./",
+    },
+    {
+      name: "University of Pittsburgh Chess Club",
+      link: "https://sites.pitt.edu/~schach/",
+    },
+    {
+      name: "ChessCo",
+      link: "https://chess.co.uk/",
+    },
+  ],
+
+  Andere: [
+    {
+      name: "KMSK Lichess",
+      link: "https://lichess.org/team/koninklijke-mechelse-sk",
+    },
+    {
+      name: "Stad Mechelen",
+      link: "https://www.mechelen.be/",
+    },
+  ],
 };
 
 export default Links_data;
--- src/pages/Links/Links.tsx
+++ src/pages/Links/Links.tsx
@@ -1,30 +1,84 @@
+import useScrollEffect from "../../ScrollEffect";
 import Links_data from "../../components/Data/Links_Data";
 import "./Links.css";
 
 const Links = () => {
+  useScrollEffect();
   return (
     <>
-      <div className="container-links">
-        <div className="subcontainer-links">
+      <div className="background-image-links">
+        <div className="hidden subcontainer-links">
           <h2>Blogs</h2>
           <ul>
-            {Links_data.Blogs.map((blog, index) => (
+            {Links_data.Blogs.map((item, index) => (
               <li key={index}>
-                <a href={blog.link} target="_blank">
-                  {blog.name}
+                <a href={item.link} target="_blank">
+                  {item.name}
                 </a>
               </li>
             ))}
           </ul>
         </div>
 
-        <div className="subcontainer-links">
+        <div className="hidden subcontainer-links">
           <h2>Federaties</h2>
           <ul>
-            {Links_data.Federaties.map((federatie, index) => (
+            {Links_data.Federaties.map((item, index) => (
               <li key={index}>
-                <a href={federatie.link} target="_blank">
-                  {federatie.name}
+                <a href={item.link} target="_blank">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="hidden subcontainer-links">
+          <h2>Internationaal</h2>
+          <ul>
+            {Links_data.Internationaal.map((item, index) => (
+              <li key={index}>
+                <a href={item.link} target="_blank">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="hidden subcontainer-links">
+          <h2>Andere clubs</h2>
+          <ul>
+            {Links_data.Clubs.map((item, index) => (
+              <li key={index}>
+                <a href={item.link} target="_blank">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="hidden subcontainer-links">
+          <h2>Leren</h2>
+          <ul>
+            {Links_data.Leren.map((item, index) => (
+              <li key={index}>
+                <a href={item.link} target="_blank">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="hidden subcontainer-links">
+          <h2>Andere</h2>
+          <ul>
+            {Links_data.Andere.map((item, index) => (
+              <li key={index}>
+                <a href={item.link} target="_blank">
+                  {item.name}
                 </a>
               </li>
             ))}
